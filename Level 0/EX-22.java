46b5ad5bb525d69111a8d538e9499fd56f1e7167
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-12-17 13:39:47 +0900

    삼각형의 완성조건 (1)
    https://school.programmers.co.kr/learn/courses/30/lessons/120889

---
+++ Level 0/EX-22.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] sides) {
+        int max = Arrays.stream(sides).max().orElse(0);
+        int sum = Arrays.stream(sides).sum();
+
+        if(sum - max > max)
+            return 1;
+        else
+            return 2;
+    }
+}
